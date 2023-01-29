82f70b3624fd5427f036bc85c71c90fc425f5604
james <jamesscalescontact@gmail.com>
2023-01-29 18:00:01 +0000

    updated project name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
 
-
-// const config = {
-//   apiKey: "AIzaSyAVOOb6qjdQM0BUd3zAAcSKRkiWyjje7W0",
-//   authDomain: "money-manager-a7894.firebaseapp.com",
-//   projectId: "money-manager-a7894",
-//   storageBucket: "money-manager-a7894.appspot.com",
-//   messagingSenderId: "423284694397",
-//   appId: "1:423284694397:web:803adfead337d33c5c4014",
-//   measurementId: "G-WYZXR401Y4"
-// };
-
 @NgModule({
   declarations: [
     AppComponent
@@ -25,10 +14,6 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule,
     AppRoutingModule,
     SharedModule,
-    // AngularFireModule.initializeApp(config),
-    // AngularFirestoreModule, // firestore
-    // AngularFireAuthModule, // auth
-    // AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
